1c770717df2b5918c45f0c127e138c070a39f49a
Sherry He <xsherryhe@gmail.com>
2021-11-08 21:03:36 -0500

    Reorganized functions and added first button

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,13 +1,29 @@
-const grid = document.querySelector('#grid-container'),
-      squares = [];
-while(grid.children.length < 256) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    squares.push(square);
-    grid.appendChild(square);
+function initializeButtons() {
+    document.querySelector('#reset-grid').addEventListener('click', resetGrid);
 }
+initializeButtons();
 
-squares.forEach(square => square.addEventListener('mouseover', changeColor));
+function setGrid(numOfSquares) {
+    const grid = document.querySelector('#grid-container'),
+          gridSize = `repeat(${numOfSquares}, ${600/numOfSquares}px)`;
+
+    grid.style.gridTemplate = `${gridSize} / ${gridSize}`;
+
+    const squares = [];
+    while(squares.length < numOfSquares * numOfSquares) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.addEventListener('mouseover', changeColor);
+        squares.push(square);
+    }
+    grid.replaceChildren(...squares);
+}
+setGrid(16);
+
+function resetGrid() {
+    document.querySelectorAll('.square')
+            .forEach(square => square.style.backgroundColor = 'white');
+}
 
 function changeColor(e) {
     e.target.style.backgroundColor = 'purple';
