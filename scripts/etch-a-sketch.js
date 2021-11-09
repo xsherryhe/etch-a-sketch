817b7760a4436dee665c7ccdcbfc5c53bacd8c50
Sherry He <xsherryhe@gmail.com>
2021-11-09 17:54:38 -0500

    Added shading pencil tool feature

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,6 +1,8 @@
 function initializeListeners() {
     document.querySelector('#clear-grid').addEventListener('click', clearGrid);
     document.querySelector('#change-grid').addEventListener('click', changeGrid);
+    document.querySelectorAll('.tool')
+            .forEach(tool => tool.addEventListener('click', changeTool));
 }
 initializeListeners();
 
@@ -32,31 +34,54 @@ function setGrid(numOfSquares) {
 }
 setGrid(51);
 
+function changeTool(e) {
+    const selectedTool = e.target.id;
+    document.querySelectorAll('.tool')
+            .forEach(tool => {
+                if(tool.id == selectedTool) tool.classList.add('selected');
+                else tool.classList.remove('selected');
+            })
+    
+    adjustPenColorSettings();
+}
+
+function adjustPenColorSettings() {
+    const penColorSettings = document.querySelector('.pen-color'),
+          selectedTool = document.querySelector('.selected.tool').id;
+    if (selectedTool == 'pen') penColorSettings.classList.remove('hidden');
+    else penColorSettings.classList.add('hidden');
+}
+
 function clearGrid() {
     document.querySelectorAll('.square')
             .forEach(square => square.style.backgroundColor = 'white');
 }
 
 function changeGrid() {
-    const numOfSquares = 101 - document.querySelector('#pen-size').value,
-          errorMessage = document.querySelector('#pen-size-error-message'),
+    const numOfSquares = 101 - document.querySelector('#tool-size').value,
+          errorMessage = document.querySelector('#tool-size-error-message'),
           sameNum = numOfSquares * numOfSquares == 
                     document.querySelector('#grid-container').children.length;
 
     if(!numOfSquares || sameNum) return;
     if(numOfSquares < 1 || numOfSquares > 100) {
-        errorMessage.textContent = 'Please select a pen size between 1 and 100.';
+        errorMessage.classList.remove('hidden');
         return;
     }
 
-    errorMessage.textContent = '';
+    errorMessage.classList.add('hidden');
     setGrid(numOfSquares);
 } 
 
 function changeColor(e) {
-    const penColor = document.querySelector('#pen-color').value;
+    const selectedTool = document.querySelector('.selected.tool').id,
+          toolColor = 
+            selectedTool == 'pen' ? document.querySelector('#pen-color').value
+          : selectedTool == 'pencil' ? darkenBy10Percent(e.target.style.backgroundColor)
+          : 'black';
+
     e.target.style.backgroundColor = 
-        penColor == 'rainbow' ? getRandomColor() : penColor;
+        toolColor == 'rainbow' ? getRandomColor() : toolColor;
 }
 
 function getRandomColor() {
@@ -64,4 +89,12 @@ function getRandomColor() {
     for(let i = 0; i < 3; i++)
         rgb.push(Math.floor(Math.random() * 256));
     return `rgb(${rgb.join(',')})`;
-}s
+}
+
+function darkenBy10Percent(color) {
+    let rgbNum = color.includes('rgb') ? 
+        +color.substring(color.indexOf('(') + 1, color.indexOf(',')) : 255;
+    rgbNum -= 25.5;
+    const rgb = [...new Array(3)].fill(rgbNum);
+    return `rgb(${rgb.join(',')})`;
+}
